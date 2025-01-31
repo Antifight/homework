747d5b927ce26c5c4241d7d3e88f7ed6a083b12f
Antifight <157541820+Antifight@users.noreply.github.com>
2025-01-31 20:43:57 +0100

    Homework 1

---
+++ domashka.py
@@ -0,0 +1,25 @@
+# 1.
+# user_name = input("Enter your name: ")
+# user_monthly_salary = float(input("Enter your monthly salary: "))
+# user_annual_salary = user_monthly_salary * 12
+# print(f"Your annual salary is: {user_annual_salary} dollars")
+
+# 2.
+# number = int(input("Enter number: "))
+# result = number % 2 == 0 and 100 <= number <= 999
+# print(result)
+
+# 3.
+# number = int(input("Enter a number: "))
+# reverse = int(str(number)[::-1])
+# print("Number is reversed:", reverse)
+
+# 4.
+# num1 = int(input("Введіть перше число: "))
+# num2 = int(input("Введіть друге число: "))
+# print("Сума:", num1 + num2) #a
+# print("Різниця:", num1 - num2) #b
+# print("Множення:", num1 * num2) #c
+# print("Ділення:", num1 / num2) #d
+# print("Залишок від поділу:", num1 % num2) #e
+# print("Перше число більше або дорівнює другому:", num1 >= num2) #f
